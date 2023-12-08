17231c8ac7dd3761102d28dbf2566bf00cd23994
Kashaf Shahraaz <kshahraaz064@gmail.com>
2023-12-08 11:08:59 +0100

    component for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import Footer from './layout/Footer';
+import Nav from './layout/Nav';
 
 function App() {
   return (
     <div data-theme="cupcake" className="App">
-      
+      <Nav />
+
+
+
+
+      <Footer />
     </div>
   );
 }
